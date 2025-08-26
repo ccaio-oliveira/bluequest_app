92a96b861999ef950056b46990e678708a3cf19b
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-08-26 10:30:34 -0300

    feat: add page challenge example

---
+++ lib/features/challenges/presentation/challenges_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ChallengesPage extends StatelessWidget {
+  const ChallengesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Desafios')),
+      body: const Center(child: Text('Lista dos seus desafios')),
+    );
+  }
+}
--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,9 +1,10 @@
 import 'package:bluequest_app/features/auth/presentation/login_page.dart';
+import 'package:bluequest_app/features/challenges/presentation/challenges_page.dart';
 import 'package:go_router/go_router.dart';
 
 final appRouter = GoRouter(
   routes: [
     GoRoute(path: '/', builder: (c, s) => const LoginPage()),
-    // GoRoute(path: '/challenges', builder: (c, s) => const ChallengesPage()),
+    GoRoute(path: '/challenges', builder: (c, s) => const ChallengesPage()),
   ],
 );
