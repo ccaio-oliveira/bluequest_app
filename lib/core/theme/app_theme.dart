ad2f3b69d55cc343d715091c1ea151f93a6e4511
Caio Oliveira <ccaio.oliveira83@gmail.com>
2025-08-26 10:46:12 -0300

    feat: comment unused variables

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -2,9 +2,9 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   static ThemeData light() {
-    const primary600 = Color(0xFF1E3A8A);
+    // const primary600 = Color(0xFF1E3A8A);
     const primary500 = Color(0xFF2563EB);
-    const primary400 = Color(0xFF60A5FA);
+    // const primary400 = Color(0xFF60A5FA);
     const accent = Color(0xFF0EA5E9);
 
     final scheme = ColorScheme.fromSeed(seedColor: primary500).copyWith(
